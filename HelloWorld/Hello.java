ef438b6222b9aa077d914d37eb22cb952afb998d
jjpnav <jjpnav@gmail.com>
2019-06-30 21:09:35 +0200

    Hello World sources

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+
+		public static void main(String[] args) {
+			System.out.println("************");
+			System.out.println("Hello World."); // say hello
+			System.out.println("************");
+		}
+}
